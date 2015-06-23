67b5a2160d0d9c3e1734447ea822b8a37706eeb0
Stavros Zavrakas <szavrakas@cambridge.org>
2015-06-23 16:30:27 +0100

    mappings

--- lean-api-j/src/main/java/resources/users.java
+++ lean-api-j/src/main/java/resources/users.java
@@ -1,13 +1,13 @@
 package resources;
 
-import java.util.Map;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import pojo.Users;
 
 @Path("/users")
 public class users {
@@ -18,10 +18,15 @@ public class users {
 		return "users";
 	}
 
+	// how to consume JSON objects.
+	// @see:
+	// http://examples.javacodegeeks.com/enterprise-java/rest/jersey/json-example-with-jersey-jackson/
+	// https://www.nabisoft.com/tutorials/java-ee/producing-and-consuming-json-or-xml-in-java-rest-services-with-jersey-and-moxy
+	// http://stackoverflow.com/questions/4486787/jackson-with-json-unrecognized-field-not-marked-as-ignorable
 	@POST
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
-	public String createUser() {
-		return "test";
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String createUser(Users users) {
+		return "users post";
 	}
 }
