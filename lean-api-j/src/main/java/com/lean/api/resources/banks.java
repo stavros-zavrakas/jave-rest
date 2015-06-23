4926bf29e1e96ea644a5a772f6753e7f45c5e586
Stavros Zavrakas <stavros.zavrakas@styloola.com>
2015-06-23 22:54:40 +0100

    moving the resources under the mail package and fix the jersey mappings

--- lean-api-j/src/main/java/com/lean/api/resources/banks.java
+++ lean-api-j/src/main/java/com/lean/api/resources/banks.java
@@ -1,4 +1,4 @@
-package resources;
+package com.lean.api.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
--- lean-api-j/src/main/java/com/lean/api/resources/users.java
+++ lean-api-j/src/main/java/com/lean/api/resources/users.java
@@ -1,4 +1,4 @@
-package resources;
+package com.lean.api.resources;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
