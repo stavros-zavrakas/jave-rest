80abb185c9f72b46feac1a9b48a7990f87796e0a
Stavros Zavrakas <stavros.zavrakas@styloola.com>
2015-07-20 23:58:42 +0100

    start integrating dao

---
+++ lean-api-j/src/main/java/com/lean/api/dao/DbContract.java
@@ -0,0 +1,8 @@
+package com.lean.api.dao;
+
+public interface DbContract {
+	public static final String HOST = "jdbc:postgresql://localhost:5432/";
+	public static final String DB_NAME = "try_postgre";
+	public static final String USERNAME = "postgres";
+	public static final String PASSWORD = "password";
+}
--- lean-api-j/src/main/java/com/lean/api/resources/users.java
+++ lean-api-j/src/main/java/com/lean/api/resources/users.java
@@ -1,5 +1,6 @@
 package com.lean.api.resources;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.ws.rs.Consumes;
@@ -8,6 +9,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+// @todo: how to handle parse errors:
+// http://stackoverflow.com/questions/14853939/how-to-handle-parser-exceptions-during-unmarshalling-of-json-data
 
 @Path("/users")
 public class users {
@@ -26,8 +31,14 @@ public class users {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String createUser(Map<String, Object> bodyMap) {
-//	public String createUser(Users users) {
-		return "users post";
+	public Response createUser(Map<String, Object> bodyMap) {
+		String firstName = (String) bodyMap.get("firstName");
+		String lastName = (String) bodyMap.get("lastName");
+
+		Map<String, Object> res = new HashMap<String, Object>();
+		res.put("firstName", firstName);
+		res.put("lastName", lastName);
+
+		return Response.ok(res).build();
 	}
 }
